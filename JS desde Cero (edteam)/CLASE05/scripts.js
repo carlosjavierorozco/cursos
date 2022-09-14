d95dca3727be9891723a4e5836f436873885ba18
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-09-14 08:11:58 -0400

    Ejercicio obtener la hora ACTUALIZADO CON LO QUE YA SE.

--- JS desde Cero (edteam)/CLASE05/scripts.js
+++ JS desde Cero (edteam)/CLASE05/scripts.js
@@ -1,1 +1,17 @@
-alert("hola mundo")
+// boton que da la hora
+function dameLaHora(){
+  let botonHora = document.getElementById('boton-hora');
+
+
+  botonHora.addEventListener('click',function(){
+      let fecha = new Date();
+      let hora = fecha.getHours();
+      let minutos = fecha.getMinutes();
+      let segundos = fecha.getSeconds();
+    alert(`Son las ${hora} y ${minutos} con ${segundos} segundos`);
+
+  })
+}
+
+dameLaHora();
+
