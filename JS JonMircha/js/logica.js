f16d365e89c667bc4a9ca4f14180214f4b8df7b3
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-10-18 08:19:04 -0400

    Ejercicio 4 - Logica de programacion

--- JS JonMircha/js/logica.js
+++ JS JonMircha/js/logica.js
@@ -40,12 +40,32 @@ const cadenaAArreglo = (cadena = "", separador = undefined) =>
       : console.info(cadena.split(separador))
 
 cadenaAArreglo('hola que tal', ' ')
+
+
+// Ejercicio 4 
+// 4) Programa una función que repita un texto X veces,
+// pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+
+
+const repetirTexto = (texto = "", veces = undefined) => {
+  if (!texto) return console.warn("No ingresaste un texto")
+  if (veces === undefined) return console.warn("No ingresaste el numero de veces a repetir el texto")
+  if (veces === 0) return console.error("No puedes repetir un texto cero veces")
+  if (Math.sign(veces) === -1) return console.error("No puedes repetir el texto con un numero negativo")
+
+  for (let i = 1; i <= veces; i++) {
+    console.info(`${i} : ${texto}`)
+  }
+
+}
+
+
+
+
+
+
+
 /*
-
- 
-
- 4) Programa una función que repita un texto X veces,
- pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
 
  5) Programa una función que invierta las palabras de una cadena de texto, pe.
  miFuncion("Hola Mundo") devolverá "odnuM aloH". 
