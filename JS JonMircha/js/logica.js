842be020b886c394a037de79e8a6334ac8c1b564
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-10-18 07:46:49 -0400

    Ejercicio 2 - Logica de programacion

--- JS JonMircha/js/logica.js
+++ JS JonMircha/js/logica.js
@@ -1,4 +1,7 @@
-//Pasar esta funcion a una funcion expresada y utilzar las funciones flechas.
+//1) Programa una función que cuente el número de caracteres de una cadena de
+//texto, pe. miFuncion("Hola Mundo") devolverá 10. Pasar esta funcion a una
+//funcion expresada y utilzar las funciones flechas.
+
 function contarCadena(cadena){
 
   if (!cadena){
@@ -8,3 +11,52 @@ function contarCadena(cadena){
   }
 
 }
+
+
+//Ejercicio 2 Programa una función que te devuelva el texto recortado según
+//el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4)
+//devolverá "Hola". 
+
+const recortarCaracter = (cadena = "", longitud = undefined) =>
+  (!cadena)
+    ? console.warn("No ingresaste nungun texto")
+    : (longitud === undefined)
+      ? console.warn("No ingresaste la longitud para recortar el texto")
+      : console.info(cadena.slice(0, longitud))
+
+recortarCaracter(,4)
+
+
+
+
+
+
+
+
+
+/*
+
+ 3) Programa una función que
+ dada una String te devuelva un Array de textos separados por cierto
+ caracter, pe. miFuncion('hola que tal', ' ') devolverá
+ ['hola', 'que', 'tal']. 
+
+ 4) Programa una función que repita un texto X veces,
+ pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+
+ 5) Programa una función que invierta las palabras de una cadena de texto, pe.
+ miFuncion("Hola Mundo") devolverá "odnuM aloH". 
+
+ 6) Programa una función para
+ contar el número de veces que se repite una palabra en un texto largo, pe.
+ miFuncion("hola mundo adios mundo", "mundo") devolverá 2. 
+
+ 7) Programa una
+ función que valide si una palabra o frase dada, es un palíndromo (que se lee
+ igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true. 
+
+ 8)
+ Programa una función que elimine cierto patrón de caracteres de un texto
+ dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1,
+ 2, 3, 4 y 5.
+*/
