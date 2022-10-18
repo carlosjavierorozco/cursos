24b102df1d5ea4607648ecb13013b1660b7bd012
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-10-18 16:32:01 -0400

    Ejercicio 10 - Logica de programacion

--- JS JonMircha/js/logica.js
+++ JS JonMircha/js/logica.js
@@ -132,13 +132,30 @@ const eleminarCaracter = (texto = "", patron = "") =>
 
 const aleatorio = () => console.info(Math.round(Math.random() * 100) + 500)
 
-aleatorio()
+//aleatorio()
 
+//Ejercicio 10
+//10) Programa una función que reciba un número y evalúe si es capicúa o no (que se
+//lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+
+const capicua = (numero = 0) => {
+  if (!numero) return console.warn("No ingresaste un numero")
+  if (typeof numero !== "number") return console.error("El dato que ingresaste no es un numero")
+
+
+  numero = numero.toString()
+  let numAlReves = numero.split("").reverse().join("")
+
+  return (numero === numAlReves)
+    ? console.info(`Si, el numero es capicua. Numero Origina: ${numero}, numero al reves: ${numAlReves}`)
+    : console.info(`No, el numero no es capicua. Numero Origina: ${numero}, numero al reves: ${numAlReves}`)
+}
+
+
+capicua(2002)
 
 /*
-10)
-Programa una función que reciba un número y evalúe si es capicúa o no (que se
-lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true. 11)
+ 11)
 Programa una función que calcule el factorial de un número (El factorial de
 un entero positivo n, se define como el producto de todos los números enteros
 positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
