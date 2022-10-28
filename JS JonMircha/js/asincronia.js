11515dc9d0e9d5779b01877068f27d309e574aa6
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-10-28 08:27:49 -0400

    callback hell

--- JS JonMircha/js/asincronia.js
+++ JS JonMircha/js/asincronia.js
@@ -1,1 +1,37 @@
-alert("asincronia")
+
+
+function cuadrado(valor, callback){
+  setTimeout(()=>{
+    callback(valor, valor * valor);
+  }, Math.random() * 3000)
+}
+
+cuadrado(0,(valor,resultado)=>{
+  console.log("inicio");
+  console.log(`
+    valor: ${valor}
+    resultado: ${resultado} `);
+
+    cuadrado(1,(valor,resultado)=>{
+      console.log(`
+        valor: ${valor}
+        resultado: ${resultado} `);
+
+      cuadrado(2,(valor,resultado)=>{
+      console.log(`
+        valor: ${valor}
+        resultado: ${resultado} `);
+        cuadrado(3,(valor,resultado)=>{
+        console.log(`
+          valor: ${valor}
+          resultado: ${resultado} `);
+          cuadrado(4,(valor,resultado)=>{
+          console.log(`
+            valor: ${valor}
+            resultado: ${resultado} `);          
+
+})
+})
+})
+})
+})
