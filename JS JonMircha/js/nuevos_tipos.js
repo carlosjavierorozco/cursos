eb44bad71c16f84f49cb9231bb3e5c8482dd9709
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-10-28 11:36:25 -0400

    los mapas en js

--- JS JonMircha/js/nuevos_tipos.js
+++ JS JonMircha/js/nuevos_tipos.js
@@ -1,5 +1,5 @@
 
-const set1 = new Set([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9])
+/*const set1 = new Set([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9])
 const set2 = new Set()
 set2.add(1)
 set2.add(1)
@@ -26,4 +26,44 @@ set1.delete(2)
 
 set1.has(5) // => true
 
-set1.clear() // limpia todo el set.
+set1.clear() // limpia todo el set.*/
+
+
+let mapa = new Map()
+mapa.set('nombre','carlos')
+mapa.set('edad',31)
+console.log(mapa)
+console.log(mapa.get('edad'))
+console.log(mapa.has('nombre')) // => true
+
+for (let [key, value] of mapa){
+  console.log(`${key}: ${value}`)
+}
+
+
+mapa.delete('nombre')
+
+
+const personaNormal = {
+  "nombre": "carlos orozco",
+  "correo": "carlosjorozco.jw@gmail.com"
+// edad: 31
+}
+
+console.log(personaNormal)
+
+// 1 = nombre, 2 = correo
+const personaMapa = new Map()
+personaMapa.set(1,"carlos javier")
+personaMapa.set(2,"carlosjorozco@yahoo.com.ve")
+
+console.log(personaMapa)
+
+const hermanas = new Map([["mayor","Mariyen"],["mediana","Pelua"],["Pequeña","Maye"]])
+
+console.log(hermanas)
+
+
+const hermanasNombres = [...hermanas.values()]
+
+console.log(hermanasNombres)
