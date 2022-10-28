309d4b3891b562a63b36ad5022f03bd65fa9f10f
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-10-28 10:05:09 -0400

    promesas

--- JS JonMircha/js/asincronia.js
+++ JS JonMircha/js/asincronia.js
@@ -1,6 +1,6 @@
 
 
-function cuadrado(valor, callback){
+/*function cuadrado(valor, callback){
   setTimeout(()=>{
     callback(valor, valor * valor);
   }, Math.random() * 3000)
@@ -34,4 +34,55 @@ cuadrado(0,(valor,resultado)=>{
 })
 })
 })
-})
+})*/
+
+
+
+/*function cuadradoPromises(valor){
+  if (typeof valor !== "number") {
+    return Promise.reject(`Error el valor "${valor}" ingresado no es un numero`)
+  }
+  
+  return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve({
+          valor,
+          resultado : valor * valor
+        })
+      }, 0 | Math.random());
+  });
+
+}
+
+
+cuadradoPromises(0)
+  .then((obj)=>{
+    //console.log(obj)
+    console.log("inicio")
+    console.log(`Promise: ${obj.valor}, ${obj.resultado}`)
+    return cuadradoPromises(1)
+  })
+  .then((obj)=>{
+    console.log(`Promise: ${obj.valor}, ${obj.resultado}`)
+    return cuadradoPromises(2)
+  })
+  .then((obj)=>{
+    console.log(`Promise: ${obj.valor}, ${obj.resultado}`)
+    return cuadradoPromises("s")
+  })
+  .then((obj)=>{
+    console.log(`Promise: ${obj.valor}, ${obj.resultado}`)
+    return cuadradoPromises(4)
+  })
+  .then((obj)=>{
+    console.log(`Promise: ${obj.valor}, ${obj.resultado}`)
+    return cuadradoPromises(5)
+  })
+  .then((obj)=>{
+    console.log(`Promise: ${obj.valor}, ${obj.resultado}`)
+    console.log("fin")
+   })
+  .catch(err => console.error(err))*/
+
+
+  
