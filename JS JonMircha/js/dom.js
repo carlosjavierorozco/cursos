c62b8a752e9a0676b931e9562e0cc5680228eb56
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-01 05:35:35 -0400

    attribute && data-attribute

--- JS JonMircha/js/dom.js
+++ JS JonMircha/js/dom.js
@@ -15,7 +15,7 @@ console.log(document.getSelection().toString())
 
 document.write("<h1>hola</h1>")*/
 
-
+/*
 let li = document.getElementsByTagName("li") // => HTMLcolecction
 let li2 = document.querySelector("li") // => HTMLElement
 let li3 = document.querySelectorAll("li") //=> nodelist
@@ -32,4 +32,39 @@ console.log(cards)
 console.log(cards2)
 console.log(cards3)
 console.log(name) 
-console.log(menu)
+console.log(menu)*/
+
+//obtener los atributos
+console.log(document.documentElement.lang)
+console.log(document.documentElement.getAttribute("lang"))
+
+
+console.log(document.querySelector(".link").href) // => http://localhost:8848/basico.html
+console.log(document.querySelector(".link").getAttribute("href")) // => basico.html
+
+document.documentElement.lang = "es-VE"
+document.documentElement.setAttribute("lang","es-MX")
+
+console.log(document.documentElement.lang)
+
+
+const $linkBasico = document.querySelector(".link")
+
+$linkBasico.setAttribute("target","_blanck")
+$linkBasico.setAttribute("rel","noopener")
+console.log($linkBasico.hasAttribute("rel"))
+console.log($linkBasico)
+$linkBasico.removeAttribute("rel")
+console.log($linkBasico.hasAttribute("rel"))
+
+
+//data attributes
+
+//obtener valores de los data attributes
+console.log($linkBasico.getAttribute("data-description"))
+console.log($linkBasico.dataset.description)
+//setear valores a los data attributes
+$linkBasico.setAttribute("data-id","link1")
+console.log($linkBasico.dataset.id)
+$linkBasico.dataset.type = "internal link"
+console.log($linkBasico.dataset.type)
