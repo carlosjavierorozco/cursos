5bcca2ee6e73e72e4fbd408364fdc05f742c8abf
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-01 05:02:17 -0400

    Nodos, elementos y selectores

--- JS JonMircha/js/dom.js
+++ JS JonMircha/js/dom.js
@@ -1,4 +1,4 @@
-console.log(document.head)
+/*console.log(document.head)
 console.log(document.body)
 console.log(document.documentElement)
 console.log(document.doctype)
@@ -13,4 +13,23 @@ setTimeout(()=>{
 console.log(document.getSelection().toString())
 },2000)
 
-document.write("<h1>hola</h1>")
+document.write("<h1>hola</h1>")*/
+
+
+let li = document.getElementsByTagName("li") // => HTMLcolecction
+let li2 = document.querySelector("li") // => HTMLElement
+let li3 = document.querySelectorAll("li") //=> nodelist
+let cards = document.getElementsByClassName("cards")
+let cards2 = document.querySelector(".cards")
+let cards3 = document.querySelectorAll(".cards")
+let name = document.getElementsByName("nombre")
+let menu = document.getElementById("menu")
+
+console.log(li)
+console.log(li2)
+console.log(li3)
+console.log(cards)
+console.log(cards2)
+console.log(cards3)
+console.log(name) 
+console.log(menu)
