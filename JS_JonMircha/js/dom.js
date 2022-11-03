25edef976fe81dde6048b61babdce2e23f05d31a
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-03 05:53:44 -0400

    Curso JavaScript: 70. DOM: Modificando Elementos (Old Style)

--- JS_JonMircha/js/dom.js
+++ JS_JonMircha/js/dom.js
@@ -292,3 +292,55 @@ cardsContent.forEach((el) => {
 });
 
 $cards.appendChild($fragmentTemp);
+
+/*
+//Curso JavaScript: 70. DOM: Modificando Elementos (Old Style)
+
+//const $cards = document.querySelector(".cards") 
+const $newCard = document.createElement("figure")
+const $cloneCards = $cards.cloneNode(true)
+$newCard.innerHTML = `
+    <img src="https://placeimg.com/200/200/any" alt="ANY">
+    <figcaption>ANY</figcaption>
+`
+
+$newCard.classList.add("card")
+
+//$cards.replaceChild($newCard,$cards.children[2])
+//$cards.insertBefore($newCard,$cards.firstElementChild)
+//$cards.removeChild($cards.lastElementChild)
+document.body.appendChild($cloneCards)
+/*
+
+
+Curso JavaScript: 71. DOM: Modificando Elementos (Cool Style)
+
+/*
+.insertAdjacent...
+  .insertAdjacentElement(position, el)
+  .insertAdjacentHTML(position, html)
+  .insertAdjacentText(position, text)
+
+Posiciones:
+  beforebegin(hermano anterior)
+  afterbegin(primer hijo)
+  beforeend(ultimo hijo)
+  afterend(hermano siguiente)
+*/
+
+const $cards = document.querySelector(".cards"),
+  $newCard = document.createElement("figure");
+
+let $contenCard = `
+  <img src="https://placeimg.com/200/200/any" alt="Any">
+  <figcaption></figcaption>
+`;
+$newCard.classList.add("card");
+
+$newCard.insertAdjacentHTML("afterbegin", $contenCard);
+$cards.insertAdjacentElement("beforeend", $newCard);
+$newCard.querySelector("figcaption").insertAdjacentText("afterbegin", "Any"); */
+//$cards.prepend($newCard); //
+//$cards.append($newCard); //
+//$cards.before($newCard);
+//$cards.after($newCard);
