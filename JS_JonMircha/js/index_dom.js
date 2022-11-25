74b27615f730f273e1b61660348ed522db3a0a07
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-25 10:30:42 -0400

    Reloj Digital JS JonMircha

--- JS_JonMircha/js/index_dom.js
+++ JS_JonMircha/js/index_dom.js
@@ -1,8 +1,10 @@
 import hamburgerMenu from "./dom/menu_amburguesa.js"
+import {digitalCLock,alarm} from "./dom/reloj.js"
 
 const d = document
 
 d.addEventListener("DOMContentLoaded", (e)=>{
   hamburgerMenu(".panel-btn",".panel",".menu a")
+  digitalCLock("#reloj","#activar-reloj","#desactivar-reloj")
 })
 
