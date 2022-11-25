b6db38d718a78bcb5af5c0dce744fe3e7a37293f
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-25 12:06:32 -0400

    alarma sonora JS jonMircha

--- JS_JonMircha/js/dom/reloj.js
+++ JS_JonMircha/js/dom/reloj.js
@@ -25,6 +25,27 @@ export function digitalCLock(clock,btnPlay,btnStop) {
   })
 }
 
-export function alarm(argument) {
+export function alarm(sound,btnPlay,btnStop) {
   
+  let alarmaTempo
+  const $alarm = d.createElement("audio")
+  $alarm.src = sound
+
+  d.addEventListener("click",(e)=>{
+    if (e.target.matches(btnPlay)) {
+      alarmaTempo = setTimeout(()=>{
+        $alarm.play()
+      },2000)
+      e.target.disabled = true
+    }
+
+    if (e.target.matches(btnStop)) {
+      $alarm.pause()
+      $alarm.currentTime = 0
+      clearTimeout(alarmaTempo)
+      d.querySelector(btnPlay).disabled = false
+    }
+
+  })
+
 }
--- JS_JonMircha/js/index_dom.js
+++ JS_JonMircha/js/index_dom.js
@@ -6,5 +6,6 @@ const d = document
 d.addEventListener("DOMContentLoaded", (e)=>{
   hamburgerMenu(".panel-btn",".panel",".menu a")
   digitalCLock("#reloj","#activar-reloj","#desactivar-reloj")
+  alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma")
 })
 
