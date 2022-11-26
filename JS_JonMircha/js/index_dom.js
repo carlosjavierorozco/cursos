994bdc8dca976efeebd3c2ebd256b0aecdc9f5b1
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-26 04:06:56 -0400

    Ejercicio cuenta regresiva con JS

--- JS_JonMircha/js/index_dom.js
+++ JS_JonMircha/js/index_dom.js
@@ -1,13 +1,15 @@
-import hamburgerMenu from "./dom/menu_amburguesa.js"
-import {digitalCLock,alarm} from "./dom/reloj.js"
-import {shortcuts, moveBall} from "./dom/eventos_teclados.js"
+import hamburgerMenu from "./dom/menu_amburguesa.js";
+import {digitalCLock,alarm} from "./dom/reloj.js";
+import {shortcuts, moveBall} from "./dom/eventos_teclados.js";
+import countdown from "./dom/cuenta_regresiva.js";
 
 const d = document
 
 d.addEventListener("DOMContentLoaded", (e)=>{
-  hamburgerMenu(".panel-btn",".panel",".menu a")
-  digitalCLock("#reloj","#activar-reloj","#desactivar-reloj")
-  alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma")
+  hamburgerMenu(".panel-btn",".panel",".menu a");
+  digitalCLock("#reloj","#activar-reloj","#desactivar-reloj");
+  alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
+  countdown("countdown","Apr 26, 2023 12:00:00","Se termino el tiempo, espero ya seas desarrollador web :)");
 })
 
 
