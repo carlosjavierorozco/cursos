8bfa50a8efc0ae8a0c1060485fa4150a41e37b60
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-26 06:14:34 -0400

    Scroll Top Bottom con JS

--- JS_JonMircha/js/index_dom.js
+++ JS_JonMircha/js/index_dom.js
@@ -2,6 +2,7 @@ import hamburgerMenu from "./dom/menu_amburguesa.js";
 import {digitalCLock,alarm} from "./dom/reloj.js";
 import {shortcuts, moveBall} from "./dom/eventos_teclados.js";
 import countdown from "./dom/cuenta_regresiva.js";
+import scrollTopButton from "./dom/boton_scroll.js";
 
 const d = document
 
@@ -10,6 +11,7 @@ d.addEventListener("DOMContentLoaded", (e)=>{
   digitalCLock("#reloj","#activar-reloj","#desactivar-reloj");
   alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
   countdown("countdown","Apr 26, 2023 12:00:00","Se termino el tiempo, espero ya seas desarrollador web :)");
+  scrollTopButton(".scroll-top-btn");
 })
 
 
