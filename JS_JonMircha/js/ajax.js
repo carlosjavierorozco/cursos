bc7de8e42881d074a1d86ef042a84647e2e67928
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-28 10:00:38 -0400

    API fetch - AJAX

--- JS_JonMircha/js/ajax.js
+++ JS_JonMircha/js/ajax.js
@@ -12,7 +12,7 @@
       console.log("exito");
       let json = JSON.parse(xhr.responseText);
 
-      console.log(json);
+      //console.log(json);
       console.log(json[0].company.name);
 
       json.forEach(el =>{
@@ -39,7 +39,41 @@
    
   })
 
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/userss")
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
 
   xhr.send();
-}())
+}());
+
+
+(function(){
+  const $fetch = document.getElementById("fetch");
+  const $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res)=>{
+    //console.log(res);
+    return res.ok ? res.json() : Promise.reject(res);
+  })
+  .then(json => {
+    json.forEach(el =>{
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetch.appendChild($fragment);
+  })
+  .catch((err)=>{
+    console.log(err)
+    let message = err.statusText || "Ocurrio un error";
+
+    if (err.status === 404) {
+      $fetch.innerHTML = `<span class="error">Error ${err.status}: recurso no encontrado</span>`
+    }else{
+      $fetch.innerHTML = `<span class="error">Error ${err.status}: ${message}</span>`
+    }
+  })
+  .finally(()=>{
+    console.log("Se ejecuta idependientemente del resultado de la promesa fetch")
+  });
+}());
