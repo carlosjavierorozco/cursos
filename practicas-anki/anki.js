6f4ebccd969c04583642589a3322837f2c454a65
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-11-29 14:22:51 -0400

    practica - clases y template uso de template y clases y herencia prototipica

--- practicas-anki/anki.js
+++ practicas-anki/anki.js
@@ -1,168 +1,8 @@
-let symbol1 = Symbol("hola")
-let symbol2 = Symbol("hola")
-
-console.log(symbol1 == symbol2)
-
-const NOMBRE = Symbol("carlos Javier")
-const SALUDAR = Symbol()
-
-const persona = {
-  [NOMBRE] : "Carlos"
-}
-
-
-console.log(persona[NOMBRE])
-
-persona[SALUDAR] = function (){
-  console.log("Hola")
-}
-
-//pasar a anki
-console.log(Object.getOwnPropertySymbols(persona))
-
-
-// traversin
-
-let $ul = document.getElementById("ul")
-let $li = document.createElement("li")
-let text = "ELEMENTO INSERTADO"
-
-/*$ul.prepend($li)
-$ul.querySelector("#ul > li:nth-child(3)").style.color = "red"
-$ul.querySelector("#ul > li:nth-child(1)").style.setProperty("background-color","blue")
-$ul.querySelector("#ul > li:nth-child(1)").insertAdjacentText("afterbegin", text)
-*/
-
-//meter en anki
-
-//prepend
-//append
-//before
-//after
-
-
-//proxies
-
-const person = {
-  nombre: "",
-  edad: 0
-}
-
-const handler = {
-  set(obj,prop,value){
-
-    if(Object.keys(obj).indexOf(prop) === -1){
-      return console.error("No existe esa propiedad")
-    }
-
-
-    if (prop === "edad" && !(/^(\d{2})$/g.test(value))) {
-      return console.error("no es una edad valida")
-    }
-
-      
-    obj[prop] = value
-
-  
-    
-  }
-
-}
-
-
-
-const carlos = new Proxy(person,handler)
-carlos.nombre = "Carlos Javier Orozco"
-carlos.twitter = "@carlosmjorozco"
-carlos.edad = 122
-console.log(carlos.nombre)
-
-
-//recorrer un arreglo con el iterador
-
-
-const num = [1,2,3,4,5,6,7,8,9]
-
-let iterator = num[Symbol.iterator]()
-
-let next = iterator.next()
-
-while(!next.done){
-  console.log(next.value)
-  next = iterator.next()
-}
-
-//con textContent
-
-
-const menu = ["inicio","galeria","blog","contacto","salir"]
-
-
-/*menu.forEach((el) => {
-  const li = document.createElement("li")
-  li.textContent = el
-  $ul.insertAdjacentElement("beforeend",li)
-})
-*/
-
-
-//con innerHTML
-
-// menu.forEach((el) =>{
-
-//   $ul.innerHTML += `<li>${el}</li>`
-
-// })
-
-
-//con fragment
-
-$fragment = document.createDocumentFragment()
-
-
-menu.forEach((el)=>{
-  let li = document.createElement("li")
-  li.textContent = el
-  $fragment.appendChild(li)
-})
-
-
-$ul.appendChild($fragment)
-
-
-
-
-//bind
-
-
-const mary = {
-    nombre: "Mary",
-    saludar: function(){
-      console.log("hola soy " + this.nombre)
-    }
-}
-
-
-const mariyen = {
-  saludar: mary.saludar.bind(mary)
-}
-
-
-//delagacion de eventos
-
-
-window.addEventListener("click",(e)=>{
-  if (e.target.matches("li")) {
-    console.log("clicaste en el un li")
-    e.stopPropagation()
-  }
-})
-
-// template 
-
-let $temp = document.getElementById("template").content
-let $frag = document.createDocumentFragment()
-let content = [
+// <template> 
+const $gallery = document.getElementById("gallery")
+const $template = document.getElementById("template").content;
+const $fragment = document.createDocumentFragment();
+let galleryContent = [
     {
       title: "lolita",
       img: "img/loli.jpg"
@@ -178,17 +18,84 @@ let content = [
     {
       title: "luffy",
       img: "img/luffy.jpg"
-    }]
+    }];
 
 
-content.forEach((el)=>{
-  $temp.querySelector("img").setAttribute("src",el.img)
-  $temp.querySelector("img").setAttribute("alt",el.title)
+galleryContent.forEach((el)=>{
+  $template.querySelector("#img").setAttribute("src", el.img);
+  $template.querySelector("#img").setAttribute("alt",el.title);
+  $template.querySelector("#fig-title").textContent = el.title;
+  const $clone = document.importNode($template,true);
+  $fragment.appendChild($clone); 
+});
 
-  let $clone = document.importNode($temp,true)
+$gallery.appendChild($fragment);
 
-  $frag.appendChild($clone)
-})
+//parametro REST
 
-document.body.prepend($frag)
+function sum(a,b,...c){
+  let result =  a + b;
+
+  c.forEach((n)=>{result+=n})
+
+  return result // me salio... muy contento xD
+}
+
+// heredar de una clase...
+
+// class Animal{
+//   constructor(nombre,raza){
+//     this.nombre = nombre;
+//     this.raza = raza;
+//   }
+
+//   saludar(){
+//     return "hola, soy " + this.nombre
+//   }
+// }
+
+// let animal = new Animal("chopper","reno");
+
+// class Perro extends Animal{
+//   constructor(nombre,raza,tamaño){
+//     super(nombre,raza)
+//     this.tamaño = tamaño;
+//   }
+// }
+
+//herencia prototipica
+
+function Animal(nombre,raza){
+  this.nombre = nombre;
+  this.raza = raza
+}
+
+function Perro(nombre,raza,tamaño){
+  this.super = Animal;
+  this.super(nombre,raza)
+  this.tamaño = tamaño;
+}
+
+Perro.prototype = new Animal();
+Perro.prototype.constructor = Perro;
+
+let rocko = new Perro("Rocko","shar-pei","mediano");
+
+//console test
+
+let a, b;
+let result = "se espera que a sea mayor que b";
+
+a = 5;
+b = 32;
+
+console.assert(a > b, {a,b,result})
+
+
+
+
+
+
+
+
 
