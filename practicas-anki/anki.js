4ce192662ad04817b5d17db9bb5aeed5fb8b198b
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-12-19 18:36:05 -0400

    practicas random *uso de template *eventos *iteradores *herencia

--- practicas-anki/anki.js
+++ practicas-anki/anki.js
@@ -16,69 +16,97 @@ let galleryContent = [
       img: "img/luffy.jpg"
     }];
 
-const plantilla = document.getElementById("template").content
-const fragmento = document.createDocumentFragment()
-const galeria = document.getElementById("gallery")
+const persona = {
+
+}
+
+const nombre = ["carlos","jesus","julio"]
+
+nombre.forEach((el,id) => {
+  persona[`id_${id+1}`] = el
+})
+
+console.log(persona)
+
+//eventos - remover eventos
+
+const boton = document.querySelector("button")
+
+const remover = ()=>{
+  console.log("removiendo")
+  boton.removeEventListener("click", remover)
+}
+
+
+boton.addEventListener("click",remover)
+
+
+// ejemplo de uso de la plantilla. 
+
+
+const template = document.querySelector("#template").content;
+const fragmento = document.createDocumentFragment();
+const gallery = document.querySelector("#gallery");
 
 
 galleryContent.forEach((el)=>{
-  plantilla.querySelector("img").setAttribute("src",el.img)
-  let clone = document.importNode(plantilla,true)
+  template.querySelector("img").setAttribute("src",el.img)
+  template.querySelector("img").setAttribute("alt",el.title)
+
+  clone = document.importNode(template,true)
 
   fragmento.append(clone)
 })
 
-galeria.append(fragmento)
+gallery.append(fragmento)
+
+//get selection
 
 
-//ejemplo de proxies 
+addEventListener("mouseup",()=>{
+  let seleccion = document.getSelection().toString()
 
-const persona = {
-  nombre: "",
-  edad: 0
+  console.log(seleccion);
+})
+
+
+// el iterador
+
+ const num = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+ const iterador = num[Symbol.iterator]()
+
+while (iterador.next().done !== true){
+  console.log(iterador.next().value)
+//  iterador.next()
 }
 
-const manejador = {
-  set(obj,prop,value){
-    if (obj.hasOwnProperty(prop) === false) {
-      return console.error("no existe el elemento")
-    }
-    obj[prop] = value
-  }
-}
+//let next = iterador.next()
+
+/*while(!next.done){
+   console.log(next.value)
+   next = iterador.next()
+}*/
 
 
+//herencia prototipica
 
-const carlos = new Proxy(persona,manejador)
-carlos.nombre = "carlos";
-carlos.correo = true;
+// function Animal(nombre,tamaño,color){
 
-console.log(carlos)
-
-
-//clases
-
-class Animal{
-  constructor(nombre,color){
-    this.nombre = nombre
-    this.color = color
-  }
-
-  saludar(){
-    return "hola"
-  }
-}
-
-class Perro extends Animal{
-  constructor(nombre,color,tamaño){
-    super(nombre,color)
-    this.tamaño = tamaño
-  }
-}
+//   this.nombre = nombre;
+//   this.tamaño = tamaño;
+//   this.color = color;
 
 
-const perro = new Animal("rocko","negro")
-const luna = new Perro("luna","negro","mediana")
+//   saludar(){
+//     return "hola"
+//   }
+// }
 
-console.log(perro.saludar())
-console.log(luna)
+
+// function Perro(nombre,tamaño,color,raza){
+//   this.super = Animal;
+//   this.super(nombre,tamaño,color)
+// }
+
+// Perro.prototype = new Animal()
+// Perro.prototype.constructor = Perro
