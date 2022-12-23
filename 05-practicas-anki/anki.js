96faa55f489c43b1b2bc95e67cd02622cf55b810
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-12-23 18:17:40 -0400

    chinesepod script

--- 05-practicas-anki/anki.js
+++ 05-practicas-anki/anki.js
@@ -17,15 +17,62 @@ let galleryContent = [
     }
     ];
 
-const template = document.getElementsByTagName("template")[0].content
-const fragmento = document.createDocumentFragment()
-const gallery = document.querySelector("div")
+// que solucion mas cutre :/ 
 
-galleryContent.forEach((el)=>{
-  template.querySelector("img").src = el.img;
-  template.querySelector("img").alt = el.title;
-  clone = document.importNode(template,true);
-  fragmento.appendChild(clone)
+let chino = ["nihao","zaijian"];
+let english = ["hello","goodbye"];
+
+
+let fila1 = [chino[0],english[0]]
+let fila2 = [chino[1],english[1]]
+let fila3 = [chino[3],english[3]]
+let fila4 = [chino[4],english[4]]
+let fila5 = [chino[5],english[5]]
+
+
+console.log(fila1)
+console.log(fila2)
+console.log(fila3)
+console.log(fila4)
+console.log(fila5)
+
+const tabla = document.createElement("table")
+let tr1 = document.createElement("tr")
+let tr2 = document.createElement("tr")
+let tr3 = document.createElement("tr")
+let tr4 = document.createElement("tr")
+let tr5 = document.createElement("tr")
+let tr6 = document.createElement("tr")
+let tr7 = document.createElement("tr")
+let tr8 = document.createElement("tr")
+
+fila1.forEach((el)=>{  
+  let td = document.createElement("td")
+  td.textContent = el
+  tr1.append(td)
+  tabla.append(tr1)
 })
 
-gallery.appendChild(fragmento)
+fila2.forEach((el)=>{  
+  let td = document.createElement("td")
+  td.textContent = el
+  tr2.append(td)
+  tabla.append(tr2)
+})
+
+fila3.forEach((el)=>{  
+  let td = document.createElement("td")
+  td.textContent = el
+  tr3.append(td)
+  tabla.append(tr3)
+})
+
+fila4.forEach((el)=>{  
+  let td = document.createElement("td")
+  td.textContent = el
+  tr4.append(td)
+  tabla.append(tr4)
+})
+
+
+document.body.appendChild(tabla)
