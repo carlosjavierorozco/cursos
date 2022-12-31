b65da534ac4f9acb544955c676ac7c8f950140a2
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2022-12-31 19:16:00 -0400

    practica en anki

--- 05-practicas-anki/anki.js
+++ 05-practicas-anki/anki.js
@@ -17,62 +17,11 @@ let galleryContent = [
     }
     ];
 
-// que solucion mas cutre :/ 
+let canvas = document.getElementById("myCanvas")
+let ctx = canvas.getContext("2d")
 
-let chino = ["nihao","zaijian"];
-let english = ["hello","goodbye"];
-
-
-let fila1 = [chino[0],english[0]]
-let fila2 = [chino[1],english[1]]
-let fila3 = [chino[3],english[3]]
-let fila4 = [chino[4],english[4]]
-let fila5 = [chino[5],english[5]]
-
-
-console.log(fila1)
-console.log(fila2)
-console.log(fila3)
-console.log(fila4)
-console.log(fila5)
-
-const tabla = document.createElement("table")
-let tr1 = document.createElement("tr")
-let tr2 = document.createElement("tr")
-let tr3 = document.createElement("tr")
-let tr4 = document.createElement("tr")
-let tr5 = document.createElement("tr")
-let tr6 = document.createElement("tr")
-let tr7 = document.createElement("tr")
-let tr8 = document.createElement("tr")
-
-fila1.forEach((el)=>{  
-  let td = document.createElement("td")
-  td.textContent = el
-  tr1.append(td)
-  tabla.append(tr1)
-})
-
-fila2.forEach((el)=>{  
-  let td = document.createElement("td")
-  td.textContent = el
-  tr2.append(td)
-  tabla.append(tr2)
-})
-
-fila3.forEach((el)=>{  
-  let td = document.createElement("td")
-  td.textContent = el
-  tr3.append(td)
-  tabla.append(tr3)
-})
-
-fila4.forEach((el)=>{  
-  let td = document.createElement("td")
-  td.textContent = el
-  tr4.append(td)
-  tabla.append(tr4)
-})
-
-
-document.body.appendChild(tabla)
+let grd = ctx.createLinearGradient(0,0,100,0)
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);
